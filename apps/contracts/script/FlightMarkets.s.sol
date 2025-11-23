16503df03a261709d8b3df4864b87c77fdb2750f
zkfriendly <zkfriendly@proton.me>
2025-11-23 05:44:14 +0100

    deploy to celo

--- apps/contracts/script/FlightMarkets.s.sol
+++ apps/contracts/script/FlightMarkets.s.sol
@@ -7,7 +7,8 @@ import {FlightDelayPredictionMarket} from "../src/FlightMarkets.sol";
 
 contract FlightDelayPredictionMarketScript is Script {
     FlightDelayPredictionMarket public market;
-    address public token = 0xf8eD259Fc6689328Dd7a04817cA8b89554dDA8af;
+    // address public token = 0xf8eD259Fc6689328Dd7a04817cA8b89554dDA8af;
+    address public token = 0x22b054128AEf0643149Af8b0cD77DA123C70B3C8;
     function setUp() public {}
 
     function run() public {
